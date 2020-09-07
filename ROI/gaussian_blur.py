a47a6eb151bd02709985bf05012ba8992f3c5bf3
Mann Doshi <manndoshi19@gmail.com>
2020-09-07 13:57:36 +0530

    ROI code added

--- ROI/gaussian_blur.py
+++ ROI/gaussian_blur.py
@@ -8,6 +8,6 @@ gaussian_kernel = [[1 / 256, 4  / 256,  6 / 256,  4 / 256, 1 / 256],
                    [6 / 256, 24 / 256, 36 / 256, 24 / 256, 6 / 256],
                    [4 / 256, 16 / 256, 24 / 256, 16 / 256, 4 / 256],
                    [1 / 256, 4  / 256,  6 / 256,  4 / 256, 1 / 256]]
-image = np.array(Image.open("blur.jpeg"))
+image = np.array(Image.open("roi.jpg"))
 gaussian_blur = Image.fromarray(np.uint8(convert(image, gaussian_kernel)))
-gaussian_blur.save("gaussian_blur.png")
+gaussian_blur.save("gaussian_output.png")
